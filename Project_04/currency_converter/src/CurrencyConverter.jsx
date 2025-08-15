aac136ece98c492dc76a915305fc5c70cf70610a
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-08-15 21:28:47 +0500

    starting out currency converter...

---
+++ Project_04/currency_converter/src/CurrencyConverter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CurrencyConverter(){
+
+    return(
+        <>
+            <h2 className="text-3xl font-bold">hello currency</h2>
+        </>
+    );
+}
+
+export default CurrencyConverter;
