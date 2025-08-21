0499351bf08092f4325620ed64ee30f208c58ca0
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-08-21 15:52:06 +0500

    currency converter project completed....

--- Project_04/currency_converter/src/CurrencyConverter.jsx
+++ Project_04/currency_converter/src/CurrencyConverter.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function CurrencyConverter(){
-
-    return(
-        <>
-            <h2 className="text-3xl font-bold">hello currency</h2>
-        </>
+    
+    return (
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://www.shutterstock.com/image-photo/stock-market-business-finance-world-600nw-2464484913.jpg')`,
+            }}
+        >
+            
+        </div>
     );
 }
 
