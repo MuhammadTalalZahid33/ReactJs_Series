f78a13462bbc0edf5157641bbb7e470e7644068e
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-08-26 14:50:11 +0500

    working on react router...

---
+++ Project_05/react_router_application/src/components/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home/Home";
+import About from "./About/About";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Contact from "./Contacts/Contact";
+
+export {Home, About, Footer, Header, Contact}
--- Project_05/react_router_application/src/main.jsx
+++ Project_05/react_router_application/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import { Home, About, Contact} from './components'
+import Layout from './Layout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>   {/* ✅ Use Layout */}
+      <Route index element={<Home />} />     {/* index = default */}
+      <Route path="about" element={<About />} />
+      <Route path="contactus" element={<Contact />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
