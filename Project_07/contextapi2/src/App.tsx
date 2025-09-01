2c1f9f32819c3dc67765f43a4d5fb19f0ec9b93f
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-09-01 15:38:16 +0500

    done... using advanced approach

--- Project_07/contextapi2/src/App.tsx
+++ Project_07/contextapi2/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
@@ -24,10 +25,12 @@ function App() {
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             {/* themeBtn */}
+            <ThemeBtn/>
           </div>
 
           <div className="w-full max-w-sm mx-auto">
             {/* CardComp */}
+            <Card/>
           </div>
         </div>
       </div>
--- Project_07/contextapi2/src/contexts/theme.ts
+++ Project_07/contextapi2/src/contexts/theme.ts
@@ -1,13 +1,17 @@
 import { createContext, useContext } from "react";
 
+// theme Context
 export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {}
 })
 
+
+// theme context provider
 export const ThemeProvider = ThemeContext.Provider
 
+// custom hook for easy access over other files
 export default function useTheme(){
     return useContext(ThemeContext)
 }
