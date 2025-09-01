3040c8dc02dc972d469b10742b5ce24cf2238e07
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-09-01 11:03:16 +0500

    main functionality implemented...

--- Project_07/contextapi2/src/App.tsx
+++ Project_07/contextapi2/src/App.tsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // function to change theme...
+  useEffect(() => {
+    document.querySelector('html')?.classList.remove("light", "dark")
+    document.querySelector('html')?.classList.add(themeMode)
+    }, [themeMode])
 
   return (
     <>
-      <div className='text-3xl text-red-700 font-semibold'>hello world</div>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* CardComp */}
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
     </>
   )
 }
